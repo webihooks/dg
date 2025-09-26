5d92cf9233c296123256c7b1547d880e428c2b16
webihooks <webihooks@gmail.com>
2025-09-27 03:19:41 +0530

    Bug Solved 1

--- login.php
+++ login.php
@@ -1,4 +1,8 @@
 <?php
+// In your server configuration or PHP file
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
 // Start the session with extended lifetime
 session_start();
 
